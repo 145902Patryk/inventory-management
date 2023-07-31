5b736ef4058d962e340162aac51031dee70f3e0c
145902Patryk <patryklaszuk98@gmail.com>
2023-07-31 09:25:50 +0200

    base.html, form update

--- apps/inventory/forms.py
+++ apps/inventory/forms.py
@@ -1,20 +1,39 @@
+import string
+
 from django import forms
+from django.forms.renderers import get_default_renderer
+from django.utils.safestring import mark_safe
 
 from .models import Tag
 
 
-class FilterForm(forms.Form):
-    DEMO_CHOICES = (
-        ("1", "Naveen"),
-        ("2", "Pranav"),
-        ("3", "Isha"),
-        ("4", "Saloni"),
-    )
+def alphabet():
+    characters = ((a, a) for a in string.ascii_uppercase)
+    return characters
 
-    name = forms.CharField(required=False)
+
+class RowCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
+
+    def render(self, name, value, attrs=None, renderer=None):
+        context = self.get_context(name, value, attrs)
+        renderer = get_default_renderer()
+        html = renderer.render(self.template_name, context)
+        html = html.replace('id="id_tags"', 'class="row" id="id_tags"')
+        html = html.replace('<div>', '<div class="col-md-4">')
+        return mark_safe(html)
+
+
+class FilterForm(forms.Form):
+    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
     tags = forms.ModelMultipleChoiceField(
         required=False,
         queryset=Tag.objects.all(),
+        widget=RowCheckboxSelectMultiple
+    )
+    starts_with = forms.ChoiceField(
+        required=False,
+        choices=alphabet(),
+        widget=forms.RadioSelect,
     )
 
     def clean_tags(self):
