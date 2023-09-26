854aee38fe37373692cc1892d5d0fd8ac4cc1e63
145902Patryk <patryklaszuk98@gmail.com>
2023-09-26 15:40:43 +0200

    loging middleware measuring time between request and response

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.inventory.middleware.LogsMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
