6901c0d818dc84d9181794ad52385b824292f45f
145902Patryk <patryklaszuk98@gmail.com>
2023-11-03 10:07:31 +0100

    Layout order

--- apps/layout/models.py
+++ apps/layout/models.py
@@ -6,9 +6,13 @@ from django.db import models
 class Layout(models.Model):
     name = models.CharField('Name', max_length=255)
     image = models.ImageField('Image', blank=True, null=True)
+    order = models.IntegerField('Order', default=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:  # noqa: D106
+        ordering = ['order']
 
 
 class Location(models.Model):
